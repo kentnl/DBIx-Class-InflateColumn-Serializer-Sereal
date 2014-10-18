581f95626866d2319905319cec8cb8c3edb5bbcd
Kent Fredric <kentfredric@gmail.com>
2014-10-19 11:22:55 +1300

    v0.001000
     - First version.

--- lib/DBIx/Class/InflateColumn/Serializer/Sereal.pm
+++ lib/DBIx/Class/InflateColumn/Serializer/Sereal.pm
@@ -4,7 +4,7 @@ use warnings;
 
 package DBIx::Class::InflateColumn::Serializer::Sereal;
 
-our $VERSION = '0.001000';
+our $VERSION = '0.001001';
 
 # ABSTRACT: Sereal based Serialization for DBIx Class Columns
 
