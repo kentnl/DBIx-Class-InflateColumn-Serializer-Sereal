8fb63457b187ab6971b46119291ba593dcf4bafa
Kent Fredric <kentfredric@gmail.com>
2014-10-19 10:12:12 +1300

    initial commit

---
+++ lib/DBIx/Class/InflateColumn/Serializer/Sereal.pm
@@ -0,0 +1,18 @@
+use 5.008;    # utf8
+use strict;
+use warnings;
+use utf8;
+
+package DBIx::Class::InflateColumn::Serializer::Sereal;
+
+our $VERSION = '0.001000';
+
+# ABSTRACT:
+
+# AUTHORITY
+
+use Moo;
+
+no Moo;
+
+1;
